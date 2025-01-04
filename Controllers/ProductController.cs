1c66d6ece8fe8fff7e185866deca50a3f466983c
Sahil Adhikari <107231644+sahiladhikari39@users.noreply.github.com>
2025-01-04 20:27:03 +0545

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using hamroStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hamroStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            //select * from products
+            var products = _context.Products.ToList();//get the list of products from the database
+            return View(products);
+        }
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace hamroStore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)//base calls constructor of parent class
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }//product.cs should be in models
+    }
+}
